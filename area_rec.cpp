5d7c95cd3909a77bcb32fea18a327cf96269a6bb
Yuvraj-Singhftw <yuvrajsinghwebd@gmail.com>
2026-07-25 17:24:20 +0530

    new changes

---
+++ area_rec.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l , b , area;
+    cout<< "enter length of rectangle : "<< endl;
+    cin>> l;
+    cout<< "enter breadth of rectangle : "<< endl;
+    cin>> b;
+    area = l*b;
+    cout<< "Area of rectangle is : "<< endl;
+    cout<< area;
+
+    return 0;
+}
