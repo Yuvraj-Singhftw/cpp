2c897cc742705f7de0042fac4abffcdec6837b50
Yuvraj-Singhftw <yuvrajsinghwebd@gmail.com>
2026-07-26 18:24:30 +0530

    hidden

--- patterns/alphabetsqr.cpp
+++ patterns/alphabetsqr.cpp
@@ -7,7 +7,7 @@ int main(){
     
     for(int i = 1; i<=r; i++){
         for(int j = 1; j<=r; j++){ //
-            cout<< char(j + 95) << " ";
+            cout<< char(j + 95) << " "; // 64 for A and 96 for a
         }
         cout<< endl;
     }
