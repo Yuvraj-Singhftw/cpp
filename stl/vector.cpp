f941a2d406eef08acaa6383e507dda15b5e0e3ed
Yuvraj-Singhftw <yuvrajsinghwebd@gmail.com>
2026-07-30 22:49:57 +0530

    vectors

---
+++ stl/vector.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>vec = {1,2,3,4,};
+
+    vec.erase(vec.begin() + 2);
+    for(int val : vec) {
+        cout<< val <<" ";
+
+    }
+    cout << endl;
+    return 0;
+    
+}
