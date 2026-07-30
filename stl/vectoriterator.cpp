27ed5f33e4d72973758505e8e3295a91f0a41213
Yuvraj-Singhftw <yuvrajsinghwebd@gmail.com>
2026-07-30 22:50:58 +0530

    iterator

---
+++ stl/vectoriterator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>vec = {1,2,3,4,};
+vector<int>::iterator it;
+
+for(it=vec.begin(); it != vec.end(); it++){
+    cout<<*(it) << " ";
+}
+    
+    cout << endl;
+    return 0;
+    
+}
