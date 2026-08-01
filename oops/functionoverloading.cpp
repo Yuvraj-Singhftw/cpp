f94cfe3dff51275e8d4a0fc5db50decd82f15c0f
Yuvraj-Singhftw <yuvrajsinghwebd@gmail.com>
2026-08-01 23:28:09 +0530

    commit

---
+++ oops/functionoverloading.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class loading {
+public:
+    void sum(int a , int b){
+        cout<<a+b<<endl;
+
+    }
+  void sum(int a , int b, int c){
+        cout<<a+b+c<<endl;
+  }
+};
+
+
+int main(){
+    
+
+    return 0;
+
+}
---
+++ oops/operatoroverloading.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class fraction {
+public:
+    
+};
+
+
+int main(){
+    
+
+    return 0;
+
+}
